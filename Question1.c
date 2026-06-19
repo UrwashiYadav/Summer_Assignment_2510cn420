b4f968305a8cc88b07c0b0e00cde377c6c4db445
unknown <Urwashi Yadav>
2026-06-19 18:19:00 +0530

    Day 1 is completed

---
+++ Question1.c
@@ -0,0 +1,16 @@
+
+//Question1
+#include<stdio.h>
+//Write a program to Calculate sum of first N natural numbers. 
+
+int main()
+{
+    int n, sum = 0;
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+    for(int i = 1; i <= n; i++)
+    {
+        sum += i;
+    }
+    printf("Sum of first %d natural numbers is: %d", n, sum);
+    return
