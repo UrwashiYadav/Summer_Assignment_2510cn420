50e60a145cc8c381abb824fd77923caa381c70d1
unknown <Urwashi Yadav>
2026-06-23 20:21:31 +0530

    Day 1 is completed

---
+++ Day1/Question4.c
@@ -0,0 +1,14 @@
+//Write a program to Count digits in a number.
+#include<stdio.h>
+int main() {
+    int n, count = 0;
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+    while(n != 0)
+    {
+        n /= 10;
+        count++;
+    }
+    printf("Number of digits: %d", count);
+    return 0;
+}
